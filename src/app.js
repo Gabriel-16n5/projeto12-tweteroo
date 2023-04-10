a84c170521f44d5a0ffaa7e1d60700951166492d
Gabriel-16n5 <gabrielgranjeia@alunos.utfpr.edu.br>
2023-04-09 21:28:36 -0300

    correções para o avaliador

--- src/app.js
+++ src/app.js
@@ -6,33 +6,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const users = [{
-    username: 'bobesponja',
-    avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png"
-},
-{
-    username: 'gabriel',
-    avatar: "https://media.discordapp.net/attachments/551138705180721167/1085360159800447027/image.png"
-}]
+let avatar = "";
 
-const tweets = [{
-	username: "bobesponja",
-  tweet: "Eu amo hambúrguer de siri!"
-},
-{
-	username: "gabriel",
-  tweet: "Eu amo amar!"
-}]
+const users = []
+
+// {
+//     username: 'gabriel',
+//     avatar: "https://media.discordapp.net/attachments/551138705180721167/1085360159800447027/image.png"
+// }
+
+const tweets = []
 
 app.get("/tweets", (request, response) => {
-
-    // const lastTweets = [
-    //     {
-	// 	username: "bobesponja",
-	// 	avatar: "https://cdn.shopify.com/s/files/1/0150/0643/3380/files/Screen_Shot_2019-07-01_at_11.35.42_AM_370x230@2x.png",
-	// 	tweet: "Eu amo hambúrguer de siri!"
-	//     }
-    // ]
     response.send(tweets)
 })
 
@@ -41,11 +26,18 @@ app.post("/tweets", (request, response) => {
 
     const validation = users.some(users => {
         if(users.username === username){
+            avatar = users.avatar;
             return true;
         }
     })
     if(validation){
-        tweets.push(request.body);
+        const lastTweet = {
+		username: username,
+		avatar: avatar,
+		tweet: request.body.tweet
+	    }
+            
+        tweets.push(lastTweet);
         return response.send("ok")
     }
     response.status(401).send("deu ruim");
